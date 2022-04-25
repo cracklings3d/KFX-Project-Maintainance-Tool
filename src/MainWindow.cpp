9f3bd85f0728d3c45770d521f283705986f6a5bb
Cracklings home <Cracklings3D@gmail.com>
2022-04-25 23:44:03 +0800

    fix github #2

---
+++ src/MainWindow.cpp
@@ -0,0 +1,79 @@
+#include <QFileDialog>
+#include <iostream>
+#include <qdebug.h>
+
+#include "./ui_MainWindow.h"
+#include "MainWindow.h"
+#include "UserConfView.h"
+
+KPM::MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+
+  connect(m_context, SIGNAL(projectFileChanged(QUrl)), this,
+          SLOT(onNewProject(QUrl)));
+}
+
+KPM::MainWindow::~MainWindow() { delete ui; }
+
+void KPM::MainWindow::on_LocateProjectButton_clicked() {
+  QFileDialog ofd(this);
+  ofd.setFileMode(QFileDialog::AnyFile);
+  ofd.setViewMode(QFileDialog::Detail);
+  ofd.setNameFilter(tr("Unreal Project Files (*.uproject)"));
+
+  //    qDebug() << dir;
+
+  if (!ofd.exec()) {
+    statusBar()->showMessage(tr("User cancelled input."));
+    return;
+  }
+
+  auto urls = ofd.selectedUrls();
+  if (urls.isEmpty()) {
+    statusBar()->showMessage(tr("Error: no input."));
+    return;
+  }
+
+  if (!validateProjectDir(urls[0])) {
+    statusBar()->showMessage(tr("Error: invalid input."));
+    return;
+  }
+
+  m_context->setProjectFile(urls[0]);
+}
+
+void KPM::MainWindow::onNewProject(QUrl new_project) {
+  if (new_project.isEmpty()) {
+    setPage(MainWindowPage::LocateProject);
+    return;
+  }
+
+  setWindowTitle(new_project.fileName(QUrl::ComponentFormattingOption::PrettyDecoded));
+  ui->ProjectRootPath->setText(new_project.path());
+  setPage(MainWindowPage::General);
+}
+
+void KPM::MainWindow::onOpenUserConfDialog() {
+  statusBar()->showMessage(tr("DEBUG MESSAGE"), 2000);
+  UserConfView userConfView(this);
+  userConfView.exec();
+}
+
+bool KPM::MainWindow::validateProjectDir(const QUrl &url) { return true; }
+
+void KPM::MainWindow::setPage(MainWindowPage page) {
+  ui->PageSelector->setCurrentIndex(static_cast<int>(page));
+}
+
+void KPM::UnrealProjectContext::setProjectFile(const QUrl &url) {
+  m_projectFile = url;
+  emit projectFileChanged(url);
+}
+
+const QUrl &KPM::UnrealProjectContext::projectFile() const { return m_projectFile; }
+
+void KPM::UnrealProjectContext::resetProjectFile() {
+  setProjectFile({});
+  emit projectFileChanged({});
+}
